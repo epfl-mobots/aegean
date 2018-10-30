17dd36a54fcff0287d17f1dee7a9d653f31b3abc
Vaios Papaspyros <b.papaspyros@gmail.com>
2018-10-30 21:42:56 +0100

    Update kmeans example

--- examples/kmeans_example.cpp
+++ examples/kmeans_example.cpp
@@ -13,8 +13,8 @@ int main()
 {
     Eigen::MatrixXd data = Eigen::MatrixXd::Random(1000, 4);
 
-    GapStatistic<KMeans<defaults::KMeansPlusPlus>> gs;
-    uint optimal_k = gs.opt_k(data, 2, 10);
+    GapStatistic<KMeans<defaults::KMeansPlusPlus>, 2, 10> gs;
+    uint optimal_k = gs.opt_k(data);
 
     KMeans<defaults::KMeansPlusPlus> km;
     std::vector<Eigen::MatrixXd> clusters = km.fit(data, optimal_k);

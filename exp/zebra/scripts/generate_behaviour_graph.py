c12d6a02dc1b24d1337ad3b774095f316f46e6ee
Vaios Papaspyros <b.papaspyros@gmail.com>
2018-11-04 13:11:21 +0100

    Implement script to visualize behaviour transitions in time

---
+++ exp/zebra/scripts/generate_behaviour_graph.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import argparse
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Generate behaviour graphs')
+    parser.add_argument('--path', '-p', type=str,
+                        help='Path to the experiment directory',
+                        default='.',
+                        required=False)
+    parser.add_argument('--behaviour-duration', '-d', type=int,
+                        help='Duration of each cluster sample', required=True)
+    parser.add_argument('--fontsize', type=int,
+                        help='Font size used in the graph labels',
+                        default=30)
+    parser.add_argument('--font', type=str,
+                        help='Font used in the graph labels',
+                        default='Arial')
+    parser.add_argument('--pen-coef', type=float,
+                        help='Width value coefficient for the edge weights/transition probabilities',
+                        default=7)
+    parser.add_argument('--node-size-coef', type=int,
+                        help='Size value coefficient for the nodes ' +
+                             '(each node gets a size that corresponds to the total time spent in it throught an experiment)',
+                        default=0.7)
+    args = parser.parse_args()
+
+    import warnings
+    warnings.filterwarnings("ignore")
+
+    tp_files = glob.glob(args.path + '/seg_*_transition_probabilities.dat')
+    bp_files = glob.glob(args.path + '/seg_*_behaviour_probabilities.dat')
+    labels = glob.glob(args.path + '/seg_*_labels.dat')
+
+    tp_files.sort()
+    bp_files.sort()
+    labels.sort()
+    assert len(tp_files) == len(bp_files) and len(tp_files) == len(labels)
+
+    for i in range(len(tp_files)):
+        print(tp_files[i], bp_files[i], labels[i])
+        transition_probs = np.loadtxt(tp_files[i])
+        behaviour_probs = np.loadtxt(bp_files[i])
+        lbls = np.loadtxt(labels[i], dtype=np.int32)
+
+        # set graph attributes
+        g = nx.DiGraph()
+        g.graph['graph'] = {
+            'labelloc': 't',
+            'bgcolor': 'transparent',
+            'fontname': args.font,
+            'fontsize': args.fontsize}
+
+        # add nodes and edges
+        g.add_nodes_from(list(range(transition_probs.shape[0])))
+        for m in range(transition_probs.shape[0]):
+            for n in range(transition_probs.shape[1]):
+                if transition_probs[m, n] == 0:
+                    continue
+                else:
+                    g.add_edge(m, n, weight=transition_probs[m, n])
+                    g[m][n]['penwidth'] = transition_probs[m, n] * args.pen_coef
+
+        # beatify nodes
+        for m in range(behaviour_probs.shape[0]):
+            g.node[m]['style'] = '"filled, setlinewidth(0)"'
+            g.node[m]['fontname'] = args.font
+            g.node[m]['fontsize'] = args.fontsize
+            g.node[m]['shape'] = 'circle'
+            g.node[m]['fillcolor'] = 'dodgerblue'
+            g.node[m]['margin'] = behaviour_probs[m] * \
+                args.node_size_coef
+        nx.draw(g)
+
+        out_path = args.path + '/seg_' + \
+            labels[i].split('/')[-1].split('_')[1] + '_graphs'
+        if not os.path.isdir(out_path):
+            os.makedirs(out_path)
+
+        # set node attributes per timestep
+        for tstep, k in enumerate(lbls.tolist()):
+            for l in range(args.behaviour_duration):
+                g_copy = g.copy()
+                g_copy.graph['graph']['label'] = 'Time spent in current state: ' + \
+                    str(l + 1) + 's'
+                g_copy.node[k]['fillcolor'] = 'green'
+                frame_name = str(tstep * args.behaviour_duration + l).zfill(6)
+                path = out_path + '/' + frame_name + '.dot'
+                nx.nx_pydot.write_dot(g_copy, path)
+        break
--- exp/zebra/scripts/visualize_trajectories.py
+++ exp/zebra/scripts/visualize_trajectories.py
@@ -13,6 +13,18 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
+def get_text(fl):
+    text = ''
+    for pair in fl:
+        if pair[0].lower() in ['alignment']:
+            text += pair[0].capitalize() + ': ' + str('{0:.2f}'.format(
+                np.asscalar(pair[1][i]) * 100)) + ' %\n'
+        if pair[0].lower() in ['interindividual']:
+            text += pair[0].capitalize() + ': ' + str('{0:.2f}'.format(
+                np.asscalar(pair[1][i]) * 360)) + ' deg\n'
+    return text
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Visualize the positions of the fish accompanied by the feature information')
@@ -26,6 +38,8 @@ if __name__ == '__main__':
                         required=True)
     parser.add_argument('--virtual', type=str,
                         help='Path to virtual trajectories')
+    parser.add_argument('--virtual-features', '-vf', nargs='+', type=str,
+                        help='List of virtual features to load')
     args = parser.parse_args()
 
     traj = np.loadtxt(args.positions)
@@ -44,6 +58,15 @@ if __name__ == '__main__':
             feature_list.append((feature_name, np.loadtxt(f)))
         else:
             warnings.warn("Skipping " + f)
+
+    vfeature_list = []
+    if args.virtual_features:
+        for f in args.virtual_features:
+            if os.path.isfile(f):
+                feature_name = f.split("_")[-1].split(".")[0]
+                vfeature_list.append((feature_name, np.loadtxt(f)))
+            else:
+                warnings.warn("Skipping " + f)
 
     if not os.path.exists(args.out_dir):
         os.makedirs(args.out_dir)
@@ -74,17 +97,18 @@ if __name__ == '__main__':
                         label='Individual ' + str(inum) + ' ' + str(
                             ('{0:.2f}'.format(np.asscalar(x)), '{0:.2f}'.format(np.asscalar(y)))))
 
-            text = ''
-            for pair in feature_list:
-                if pair[0].lower() in ['alignment']:
-                    text += pair[0].capitalize() + ': ' + str('{0:.2f}'.format(
-                        np.asscalar(pair[1][i]) * 100)) + ' %\n'
-                if pair[0].lower() in ['interindividual']:
-                    text += pair[0].capitalize() + ': ' + str('{0:.2f}'.format(
-                        np.asscalar(pair[1][i]) * 360)) + ' deg\n'
+            text = get_text(feature_list)
+            text = 'Living agents\n' + text
             plt.text(0.4, 0.1, text, horizontalalignment='left',
                      verticalalignment='center', transform=ax.transAxes)
             fig.patch.set_visible(False)
+
+            if args.virtual_features:
+                vtext = get_text(vfeature_list)
+                vtext = 'Mixed agents\n' + vtext
+                plt.text(0.4, 0.0, vtext, horizontalalignment='left',
+                         verticalalignment='center', transform=ax.transAxes)
+                fig.patch.set_visible(False)
 
         if args.virtual:
             x = vtraj[i, 0]
